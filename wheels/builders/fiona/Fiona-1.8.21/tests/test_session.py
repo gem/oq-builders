f31a2f0fb90bbacfedf80f1ba18fae3cac9b81ac
Antonio Ettorre <vot4anto@gmail.com>
2022-09-30 16:24:46 +0200

    remove folder Fiona-1.8.21

--- wheels/builders/fiona/Fiona-1.8.21/tests/test_session.py
+++
@@ -1,9 +0,0 @@
-"""Tests of the ogrext.Session class"""
-
-import fiona
-
-
-def test_get(path_coutwildrnp_shp):
-    with fiona.open(path_coutwildrnp_shp) as col:
-        feat3 = col.get(2)
-        assert feat3['properties']['NAME'] == 'Mount Zirkel Wilderness'
