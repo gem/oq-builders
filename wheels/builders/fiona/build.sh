57e6378712dd5a98401bb822ddf7f3c471b8245c
Antonio Ettorre <vot4anto@gmail.com>
2022-09-30 15:18:49 +0200

    typo

--- wheels/builders/fiona/build.sh
+++ wheels/builders/fiona/build.sh
@@ -9,8 +9,8 @@ fi
 echo "::group::Install a virtualenv"
   pwd
   ls -lrt
-  source /wheels/builders/fiona/multibuild/common_utils.sh
-  source /wheels/builders/fiona/multibuild/travis_steps.sh
+  source wheels/builders/fiona/multibuild/common_utils.sh
+  source wheels/builders/fiona/multibuild/travis_steps.sh
   python3 -m pip install virtualenv
   before_install
 echo "::endgroup::"
