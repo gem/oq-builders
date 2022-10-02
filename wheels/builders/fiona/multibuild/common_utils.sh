43cb451de0371d76138242b23b6c7f6ce2d93ffb
Antonio Ettorre <vot4anto@gmail.com>
2022-10-02 19:39:51 +0200

    use clone Fiopna

--- wheels/builders/fiona/multibuild/common_utils.sh
+++ wheels/builders/fiona/multibuild/common_utils.sh
@@ -282,19 +282,7 @@ function fetch_unpack {
 function clean_code {
     local repo_dir=${1:-$REPO_DIR}
     local build_commit=${2:-$BUILD_COMMIT}
-    [ -z "$repo_dir" ] && echo "repo_dir not defined" && exit 1
-    [ -z "$build_commit" ] && echo "build_commit not defined" && exit 1
-    # The package $repo_dir may be a submodule. git submodules do not
-    # have a .git directory. If $repo_dir is copied around, tools like
-    # Versioneer which require that it be a git repository are unable
-    # to determine the version.  Give submodule proper git directory
-    fill_submodule "$repo_dir"
-    (cd $repo_dir \
-        && git fetch origin --tags \
-        && git checkout $build_commit \
-        && git clean -fxd \
-        && git reset --hard \
-        && git submodule update --init --recursive)
+    git clone -b ${build_commit} ${repo_dir} 
 }
 
 function build_wheel_cmd {
