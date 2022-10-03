696b69393649a0ea97f10bb226a17948a8ce747a
Antonio Ettorre <vot4anto@gmail.com>
2022-10-03 09:26:55 +0200

    remove build library

--- wheels/builders/fiona/build.sh
+++ wheels/builders/fiona/build.sh
@@ -17,11 +17,6 @@ echo "::group::Install a virtualenv"
   before_install
 echo "::endgroup::"
 
-echo "::group::Build Library"
-  pwd
-  bash -x wheels/builders/fiona/multibuild/library_builders.sh
-echo "::endgroup::"
-
 echo "::group::Build wheel"
   pwd
   gh_clone $REPO_GIT $BUILD_COMMIT
