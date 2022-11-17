613239497f2a95f256657b40c8b620c803afb3ff
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2022-11-17 21:43:07 +0100

    adjust folder

--- ci/windows_package.sh
+++ ci/windows_package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/runner/work/oq-builders/oq-builders/installers/windows/nis
+cd /home/runner/work/oq-builders/oq-builders/installers/windows/nsis
 # build the docker to use for installer
 docker build --build-arg uid=$(id -u) --rm=true -t wine -f docker/Dockerfile docker
