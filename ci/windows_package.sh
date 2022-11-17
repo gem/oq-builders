fdce8ff99a9bdf162d7d955a3a81011d0c030cc1
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2022-11-17 22:28:47 +0100

    add DEBUG

--- ci/windows_package.sh
+++ ci/windows_package.sh
@@ -3,5 +3,5 @@ cd /home/runner/work/oq-builders/oq-builders/installers/windows/nsis
 # build the docker to use for installer
 docker build --build-arg uid=$(id -u) --rm=true -t wine -f docker/Dockerfile docker
 #
-docker run -e GEM_SET_BRANCH=master -e GEM_SET_BRANCH_TOOLS=master -v $(pwd):/io --rm wine  /io/docker/build.sh
+docker run -e GEM_SET_DEBUG=1 -e GEM_SET_BRANCH=master -e GEM_SET_BRANCH_TOOLS=master -v $(pwd):/io --rm wine  /io/docker/build.sh
 
