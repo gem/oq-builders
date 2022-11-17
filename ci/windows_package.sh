04c6c3ef0ef335374c2c991b1afb322ffd4630b6
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2022-11-17 21:23:39 +0100

    set folders

--- ci/windows_package.sh
+++ ci/windows_package.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-cd ~/oq-builders/installers/windows/nis
+cd /home/runner/work/oq-builders/oq-builders/installers/windows/nis
+#
 docker build --build-arg uid=$(id -u) --rm=true -t wine -f docker/Dockerfile docker
