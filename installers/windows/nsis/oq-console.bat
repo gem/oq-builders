b4e248151c627477567035a460f1c9cc8dd195c6
gem_ci_pipeline <aettorre@mercalli.ws.gem.lan>
2023-04-17 11:21:50 +0200

    remove empty line

--- installers/windows/nsis/oq-console.bat
+++ installers/windows/nsis/oq-console.bat
@@ -3,14 +3,12 @@ setlocal
 set mypath=%~dp0
 set PATH=%mypath%\python3;%mypath%\python3\Scripts;%PATH%
 set NUMBA_CACHE_DIR=%TEMP%\openquake
-
 if not exist python3\pycached (
    echo Building python cache. This may take a while.
    echo Please wait ...
    python.exe -m compileall -qq .
    copy /y nul python3\pycached >nul
 )
-
 echo OpenQuake environment loaded
 echo To see versions of installed software run 'pip freeze'
 echo To run OpenQuake use 'oq' and 'oq engine'
