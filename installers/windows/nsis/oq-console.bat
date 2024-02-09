e8b0e49b68a7811871feefc57aa5245a181837ce
antonioettorre <antonio.ettorre@globalquakemodel.org>
2024-02-09 09:51:02 +0100

    remove pip freeze from console, no devel installation

--- installers/windows/nsis/oq-console.bat
+++ installers/windows/nsis/oq-console.bat
@@ -12,7 +12,6 @@ if not exist python3\pycached (
 )
 
 echo OpenQuake environment loaded
-echo To see versions of installed software run 'pip freeze'
 echo To run OpenQuake use 'oq' and 'oq engine'
 cmd /k
 
