8728f09e7f27779abdd2adf6b3fa0f1af13abe08
Antonio Ettorre <vot4anto@gmail.com>
2026-07-15 06:55:10 +0200

    Initializes django_admin in oq.bat

--- installers/windows/nsis/oq-engine-console.bat
+++ installers/windows/nsis/oq-engine-console.bat
@@ -6,6 +6,7 @@ set NUMBA_CACHE_DIR=%TEMP%\openquake
 set NUMEXPR_MAX_THREADS=8
 set GMT_LIBRARY_PATH="%mypath%GMT\bin"
 set GMT_SHARED="%mypath%GMT\share"
+set DJANGO_SETTINGS_MODULE=openquake.server.settings
 
 if not exist python3\pycached (
    echo Building python cache. This may take a while.
@@ -14,6 +15,8 @@ if not exist python3\pycached (
    copy /y nul python3\pycached >nul
 )
 
+echo Please wait while the system initializes.
+python3\Scripts\django-admin.exe openquake_engine_postinstall django_gem_taxonomy
 echo OpenQuake environment loaded
 echo To run OpenQuake use 'oq' and 'oq engine'
 cmd /k
